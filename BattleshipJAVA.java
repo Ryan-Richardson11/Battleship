407939d211a120f8ff18adbf60c3823fff62cd16
unknown <ryanrich811@gmail.com>
2023-09-04 09:01:33 -0400

    Unsaved Changes

--- BattleshipJAVA.java
+++ BattleshipJAVA.java
@@ -7,8 +7,6 @@ public class BattleshipJAVA {
     String columnList = "    0   1   2   3   4   5   6   7   8   9";
 
 
-
-
     public void createGrid() {
         for(int i = 0; i<gridSize; i++) {
             for(int j = 0; j<gridSize; j++) {
@@ -17,5 +15,19 @@ public class BattleshipJAVA {
         }
     }
 
+    public void drawBoard() {
+        System.out.println(columnList);
+        System.out.println("  +---+---+---+---+---+---+---+---+---+---+");
+        for(int i = 0; i<gridSize; i++) {
+            for(int j = 0; j<gridSize; j++) {
+                System.out.println(" " + grid[i][j] + " |");
+            System.out.println("  +---+---+---+---+---+---+---+---+---+---+");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        drawBoard();    
+    }
 
 }
