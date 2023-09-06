8ddc63a78bbd7defbce5b908a39d6e4c53354a07
Ryan Richardson <ryanrich811@gmail.com>
2023-09-06 16:57:47 -0400

    initialize and setupboard function for battle ship Java conversion

--- BattleshipJAVA.java
+++ BattleshipJAVA.java
@@ -1,34 +1,55 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class BattleshipJAVA {
     int gridSize = 10;
     String[][] grid = new String[gridSize][gridSize];
-    {
+    String column_list = "    0   1   2   3   4   5   6   7   8   9";
+    int numShips = 5;
+
+    public void initializeBoard() {
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
-                grid[i][j] = "";
+                grid[i][j] = " ";
             }
         }
     }
-    int numShips = 5;
-    String columnList = "    0   1   2   3   4   5   6   7   8   9";
 
     public void drawBoard() {
-        System.out.println(columnList);
+        System.out.println(column_list);
         System.out.println("  +---+---+---+---+---+---+---+---+---+---+");
 
         for (int i = 0; i < gridSize; i++) {
-            System.out.println(i + " |");
+            System.out.print(i + " |");
             for (int j = 0; j < gridSize; j++) {
-                System.out.println(" " + grid[i][j] + " |");
+                System.out.print(" " + grid[i][j] + " |");
             }
+            System.out.println();
             System.out.println("  +---+---+---+---+---+---+---+---+---+---+");
+        }
+    }
+
+    public void setUpBoard() {
+        int randomShips = 0;
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                grid[i][j] = ".";
+            }
+        }
+        while (randomShips > numShips) {
+            randomRow = 
+            randomColumn = 
+            if (grid[randomRow][randomColumn] != "S") {
+                grid[randomRow][randomColumn] == "S"
+                randomShips += 1;
+            }
         }
     }
 
     public static void main(String[] args) {
         BattleshipJAVA game = new BattleshipJAVA();
+        game.initializeBoard();
         game.drawBoard();
+        game.setUpBoard();
     }
-
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,33 @@
+public class BattleshipJAVA {
+// int grid_size = 10;
+// String[][] grid = new String[grid_size][grid_size];
+// String column_list = " 0 1 2 3 4 5 6 7 8 9";
+
+// public void initializeBoard() {
+// for (int i = 0; i < grid_size; i++) {
+// for (int j = 0; j < grid_size; j++) {
+// grid[i][j] = " ";
+// }
+// }
+// }
+
+// public void drawBoard() {
+// System.out.println(column_list);
+// System.out.println(" +---+---+---+---+---+---+---+---+---+---+");
+
+// for (int i = 0; i < grid_size; i++) {
+// System.out.print(i + " |");
+// for (int j = 0; j < grid_size; j++) {
+// System.out.print(" " + grid[i][j] + " |");
+// }
+// System.out.println();
+// System.out.println(" +---+---+---+---+---+---+---+---+---+---+");
+// }
+// }
+
+// public static void main(String[] args) {
+// BattleshipJAVA game = new BattleshipJAVA();
+// game.initializeBoard();
+// game.drawBoard();
+// }
+// }
